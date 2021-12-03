2ce9340a9e6ecb5c9d4dacb0bc6bd054e7c01a6b
jrubino22 <93841868+jrubino22@users.noreply.github.com>
2021-12-02 16:46:34 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,75 @@
-const rpsArray = ["Rock", "Paper", "Scissors"]
+const gameArray = ["Rock", "Paper", "Scissors"]
+
+
+
+let opponent = document.querySelector("#opponent")
+
+const btns = document.querySelectorAll(".button")
+
+let result = document.querySelector("#result")
+
+let playerChoice;
+
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (b) {
+        const classes = b.currentTarget.classList;
+
+        let targetNumber = Math.floor(Math.random() * gameArray.length)
+        let targetPlay = gameArray[targetNumber]
+        opponent.textContent = ("opponent: " + targetPlay)
+
+        if (classes.contains("rock")) {
+            playerChoice = "Rock"
+
+            if (targetNumber === 1) {
+                result.textContent = ("You Lose :(")
+                result.style.color = "red"
+            }
+            else if (targetNumber === 2) {
+                result.textContent = ("You Win! :)")
+                result.style.color = "green"
+            }
+            else {
+                result.textContent = ("It's a Draw!")
+                result.style.color = "brown"
+            }
+
+        }
+        else if (classes.contains("paper")) {
+            playerChoice = "Paper"
+
+            if (targetNumber === 1) {
+                result.textContent = ("It's a Draw!")
+                result.style.color = "brown"
+            }
+            else if (targetNumber === 2) {
+                result.textContent = ("You Lose :(")
+                result.style.color = "red"
+            }
+            else {
+                result.textContent = ("You Win! :)")
+                result.style.color = "green"
+            }
+        }
+
+        else if (classes.contains("scissors")) {
+            playerChoice = "Scissors"
+
+            if (targetNumber === 1) {
+                result.textContent = ("You Win! :)")
+                result.style.color = "green"
+            }
+            else if (targetNumber === 2) {
+                result.textContent = ("It's a Draw!")
+                result.style.color = "brown"
+            }
+            else {
+                result.textContent = ("You Lose :(")
+                result.style.color = "red"
+            }
+        }
+
+    })
+})
+
